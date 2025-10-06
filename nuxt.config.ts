3d5b3ff32254eb55841b69705d7875af17fec581
nicolastuomaala <nicolas.tuomaala00@gmail.com>
2025-10-06 21:44:20 +0300

    asd

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
             meta: [
                 { charset: 'utf-8' },
                 { name: "viewport", content: "width=device-width, initial-scale=1, maximum-scale=1" },
-                { hid: 'description', name: 'description', content: 'Reinvent Yellow - From Nordic roots to global reach. We unite creativity, capital, and technology to build stories that travel.' }
+                { hid: 'description', name: 'description', content: 'We unite creativity, capital, and technology to build stories that travel.' }
             ],
             link: [
                 { rel: 'icon', type: 'image/svg', href: '/logo.svg' },
