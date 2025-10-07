7c7d03274ed35d997f05467d229416de3f77ab83
nicolastuomaala <nicolas.tuomaala00@gmail.com>
2025-10-07 13:51:52 +0300

    changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
     app: {
         head: {
             titleTemplate: '%s - Sales company, financing partner, tech innovator, story enabler.',
-            title: 'Reinvent Yellow',
+            title: 'Reinvent Sales',
             meta: [
                 { charset: 'utf-8' },
                 { name: "viewport", content: "width=device-width, initial-scale=1, maximum-scale=1" },
